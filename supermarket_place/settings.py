b753bb9baef76ccd7ff8461ec89804922dc83211
Scallss <pascalhafidz2005@gmail.com>
2024-09-18 00:21:33 +0700

    added csrf trusted origins

--- supermarket_place/settings.py
+++ supermarket_place/settings.py
@@ -128,3 +128,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://pascal-hafidz-supermarketplace2.pbp.cs.ui.ac.id", "https://pascal-hafidz-supermarketplace2.pbp.cs.ui.ac.id"]
